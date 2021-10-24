905817066cb28d004a095b57625d69695cd333ac
Aminul Islam <aminulds61@gmail.com>
2021-10-24 09:36:28 +0600

    add mouseleve event on menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -2,8 +2,8 @@ import "./menu.scss"
 
 export default function Menu({openMenu, setOpenMenu}) {
   return (
-    <div className={"menu " + (openMenu && "active")}>
-      <ul>
+    <div onMouseLeave={() => setOpenMenu(false)} className={"menu " + (openMenu && "active")}>
+      <ul >
         <li onClick={() => setOpenMenu(false)}><a href="#intro">Home</a></li>
         <li onClick={() => setOpenMenu(false)}><a href="#portfolio">Portfolio</a></li>
         <li onClick={() => setOpenMenu(false)}><a href="#work">Work</a></li>
