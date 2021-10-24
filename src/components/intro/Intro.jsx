6bb8fd56af21cfc1bb50502e575b0be71be01171
Aminul Islam <aminulds61@gmail.com>
2021-10-24 09:20:20 +0600

    added intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,35 @@
 import "./intro.scss"
+import {init} from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const nameRef = useRef();
+  useEffect(() => {
+    init(nameRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ['HTML CSS JavaScript', 'Git Bootstrap Sass', 'React MUI']
+    })
+  }, [])
   return (
     <div className="intro" id="intro">
-      
+      <div className="left">
+        <div className="imageContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Aminul Islam</h1>
+          <h3>Frontend Developer</h3>
+          <h5>Skill: <span ref={nameRef}></span></h5>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   )
 }
