2d831ff42bba35cec5c894e18ebff39633ccb3ff
Aminul Islam <aminulds61@gmail.com>
2021-10-25 06:53:46 +0600

    portfolio section done

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,4 @@
-import { Mail, Person } from "@mui/icons-material"
+import { GitHub, LinkedIn } from "@mui/icons-material"
 import "./topbar.scss"
 
 export default function Topbar({openMenu, setOpenMenu}) {
@@ -8,12 +8,8 @@ export default function Topbar({openMenu, setOpenMenu}) {
         <div className="left">
           <a href="#intro" className="logo">AMINUL</a>
           <div className="itemContainer">
-            <Person className="icon"></Person>
-            <span>+880 1743724256</span>
-          </div>
-          <div className="itemContainer">
-            <Mail className="icon"></Mail>
-            <span>aminulds61@gmail.com</span>
+            <a href="https://github.com/aminulds"><GitHub className="icon"></GitHub></a>
+            <a href="https://www.linkedin.com/in/aminulds"><LinkedIn className="icon"></LinkedIn></a>
           </div>
         </div>
         
