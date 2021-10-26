35d9291f577ec5476be1e8f9abf53c889ad327d9
Aminul Islam <aminulds61@gmail.com>
2021-10-26 08:13:35 +0600

    Testimonial Section done

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,9 +1,30 @@
+import { testimonialData } from "../../data/testimonialData"
 import "./testimonial.scss"
+
 
 export default function Testimonial() {
   return (
     <div className="testimonial" id="testimonial">
-      
+      <h1>Testimonials</h1>
+      <div className="container">
+        {/* Show Data */}
+        {testimonialData.map(data => (
+        <div className={data.featured ? "card featured": "card"}>
+          <div className="top">
+            <img className="left" src="assets/right-arrow.png" alt="" />
+            <img className="user" src={data.img} alt="" />
+            <img className="right" src={data.icon} alt="" />
+          </div>
+          <div className="center">
+            {data.desc}
+          </div>
+          <div className="bottom">
+            <h3>{data.name}</h3>
+            <h4>{data.title}</h4>
+          </div>
+        </div>
+        ))}
+      </div>
     </div>
   )
 }
---
+++ src/data/testimonialData.js
@@ -0,0 +1,27 @@
+export const testimonialData = [
+  {
+    id: 1,
+    name: "Esteban Quinones",
+    title: "Senior Developer",
+    img: "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto,t_profile_small/v1/profile/photos/11579356/original/68206_470002779717150_1031918614_n.jpg",
+    icon: "https://cdn-icons-png.flaticon.com/512/174/174857.png",
+    desc: "excellent work! I really recommend him.",
+  },
+  {
+    id: 2,
+    name: "Jackson Earngey",
+    title: "Development Manager",
+    img: "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto,t_profile_small/v1/profile/photos/62991033/original/1493035817488_facebook20170424-1427-13y9ail.jpg",
+    icon: "https://cdn-icons-png.flaticon.com/512/174/174857.png",
+    desc: "It was a great pleasure working with Aminulds , very skillful he succeed to help me in my python project and mad me satisfied with what he has done , and i am really looking for working with him for the comming projects.",
+    featured: true,
+  },
+  {
+    id: 3,
+    name: "Gowa Shington",
+    title: "Software Developer",
+    img: "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto,t_profile_small/v1/attachments/profile/photo/690f5c45b18ed3c62151d4504ecba69a-1632685207036/31f24112-c69f-4e98-81c7-a333030e3eaf.jpg",
+    icon: "https://cdn-icons-png.flaticon.com/512/174/174857.png",
+    desc: "Unbelievable results - super fast to respond and deliver. A great talent",
+  },
+];
